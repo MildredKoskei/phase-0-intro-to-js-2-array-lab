d91c2bd2860791200a2c33174516ff3bdc2f2ada
Mildred Chepkoech Koskei <mildredkoskei01@gmail.com>
2022-08-17 16:37:07 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+    console.log(cats);
+
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+    
+}
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const cats = ["Milo", "Otis", "Garfield"];
+    const newArray = [...cats, "Broom"];
+    return newArray;
+}
+function prependCat(name){
+    const cats = ["Milo", "Otis", "Garfield"];
+    const newArray = ["Arnold", ...cats];
+    return newArray;
+}
+function removeLastCat(){
+    const cats = ["Milo", "Otis", "Garfield"];
+    const newArray= cats.slice(0, -1);
+    return newArray;
+}
+function removeFirstCat(){
+    const cats = ["Milo", "Otis", "Garfield"];
+    const newArray =  cats.slice(1);
+    return newArray;
+}
